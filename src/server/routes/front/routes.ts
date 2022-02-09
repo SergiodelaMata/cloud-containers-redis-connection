3e72805a19e3d9438f179c5e624d19b87901b459
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-09 19:01:12 +0100

    :bug: Corrected that happened when at the session it wasn't have any email.

--- src/server/routes/front/routes.ts
+++ src/server/routes/front/routes.ts
@@ -4,14 +4,23 @@ import {createClient} from "redis";
 const router: Router = express.Router();
 
 router.get("/logged/:email", async (req: Request, res: Response) => {
-  const client = createClient();
-  await client.connect();
-  const response = await client.get(req.params.email);
-  var value = JSON.parse(response);
-  if(!value)
+  var value;
+  if(req.params.email != undefined)
+  {
+    const client = createClient();
+    await client.connect();
+    const response = await client.get(req.params.email);
+    value = JSON.parse(response);
+    if(!value)
+    {
+      value = {status: "empty"};
+    }
+  }
+  else
   {
     value = {status: "empty"};
   }
+  
   res.status(200).send(value);
 });
 
@@ -23,8 +32,16 @@ router.post("/login", async (req: Request, res: Response) => {
     rol : req.body.rol
   };
   await client.set(req.body.email, JSON.stringify(value));
-  await client.expire(req.body.rol, 60*60);//El tiempo de expiración va en segundos
-  res.status(200).send([{status: true}]);
+  await client.expire(req.body.email, 60*60);//El tiempo de expiración va en segundos
+  res.status(200).send({status: true});
 });
+
+router.delete("/logout/:email", async (req: Request, res: Response) => {
+  const client = createClient();
+  await client.connect();
+  await client.expire(req.params.email, 1);//El tiempo de expiración va en segundos
+  res.status(200).send({status: true});
+});
+
 
 export default router;
