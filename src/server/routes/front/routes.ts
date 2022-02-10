fc401a40086f98ad8955da91bae6c7b8bdfee08f
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-11 00:56:28 +0100

    :sparkles: Added x-type headers.

--- src/server/routes/front/routes.ts
+++ src/server/routes/front/routes.ts
@@ -20,7 +20,10 @@ router.get("/logged/:email", async (req: Request, res: Response) => {
   {
     value = {status: "empty"};
   }
-  
+  res.header("Content-Type", "application/json");
+  res.header("X-version","1");
+  res.header("X-sender","redis");
+  res.header("X-destination","enrouting");
   res.status(200).send(value);
 });
 
@@ -33,6 +36,10 @@ router.post("/login", async (req: Request, res: Response) => {
   };
   await client.set(req.body.email, JSON.stringify(value));
   await client.expire(req.body.email, 60*60);//El tiempo de expiración va en segundos
+  res.header("Content-Type", "application/json");
+  res.header("X-version","1");
+  res.header("X-sender","redis");
+  res.header("X-destination","enrouting");
   res.status(200).send({status: true});
 });
 
@@ -40,6 +47,10 @@ router.delete("/logout/:email", async (req: Request, res: Response) => {
   const client = createClient();
   await client.connect();
   await client.expire(req.params.email, 1);//El tiempo de expiración va en segundos
+  res.header("Content-Type", "application/json");
+  res.header("X-version","1");
+  res.header("X-sender","redis");
+  res.header("X-destination","enrouting");
   res.status(200).send({status: true});
 });
 
