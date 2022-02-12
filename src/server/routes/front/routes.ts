2a820bdcae4829c6640116757d778899ef0cac2a
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-12 12:34:29 +0100

    :sparkles: Added functionalities to block, unblock and check if a user is blocked.

--- src/server/routes/front/routes.ts
+++ src/server/routes/front/routes.ts
@@ -54,5 +54,54 @@ router.delete("/logout/:email", async (req: Request, res: Response) => {
   res.status(200).send({status: true});
 });
 
+router.put("/block/:email", async (req: Request, res: Response) => {
+  const client = createClient();
+  await client.connect();
+  const value = {
+    email : req.body.email,
+  };
+  await client.set(req.params.email+"-block", JSON.stringify(value));
+  await client.expire(req.params.email, 24*60*60);//El tiempo de expiración va en segundos
+  res.header("Content-Type", "application/json");
+  res.header("X-version","2");
+  res.header("X-sender","redis");
+  res.header("X-destination","enrouting");
+  res.status(200).send({status: true});
+});
+
+router.get("/isBlock/:email", async (req: Request, res: Response) => {
+  var value;
+  if(req.params.email != undefined)
+  {
+    const client = createClient();
+    await client.connect();
+    const response = await client.get(req.params.email + "-block");
+    value = JSON.parse(response);
+    if(!value)
+    {
+      value = {status: "empty"};
+    }
+  }
+  else
+  {
+    value = {status: "empty"};
+  }
+  res.header("Content-Type", "application/json");
+  res.header("X-version","2");
+  res.header("X-sender","redis");
+  res.header("X-destination","enrouting");
+  res.status(200).send(value);
+});
+
+router.delete("/removeBlock/:email", async (req: Request, res: Response) => {
+  const client = createClient();
+  await client.connect();
+  await client.expire(req.params.email+"-block", 1);//El tiempo de expiración va en segundos
+  res.header("Content-Type", "application/json");
+  res.header("X-version","2");
+  res.header("X-sender","redis");
+  res.header("X-destination","enrouting");
+  res.status(200).send({status: true});
+});
 
 export default router;
